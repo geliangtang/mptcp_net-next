058758518b460ff177579946da9a34a68372e750
Geliang Tang <geliang.tang@suse.com>
2023-06-25 20:33:56 +0800

    selftests/bpf: Add bpf_stale scheduler & test
    This patch implements the setting a subflow as stale/unstale in BPF MPTCP
    scheduler, named bpf_stale. The staled subflow id will be added into a
    map in sk_storage.
    
    Two helper mptcp_subflow_set_stale() and mptcp_subflow_clear_stale() are
    added.
    
    In this test, subflow 1 is set as stale in bpf_stale_data_init(). Each
    subflow is checked whether it's a stale one in bpf_stale_get_subflow() to
    select a unstale subflow to send data.
    
    This patch adds the bpf_stale scheduler test: test_stale(). Use sysctl to
    set net.mptcp.scheduler to use this sched. Add two veth net devices to
    simulate the multiple addresses case. Use 'ip mptcp endpoint' command to
    add the new endpoint ADDR_2 to PM netlink. Send data and check bytes_sent
    of 'ss' output after it to make sure the data has been only sent on ADDR_1
    since ADDR_2 is set as stale.
    
    Signed-off-by: Geliang Tang <geliang.tang@suse.com>

--- tools/testing/selftests/bpf/prog_tests/mptcp.c
+++ tools/testing/selftests/bpf/prog_tests/mptcp.c
@@ -20,6 +20,7 @@
 #include "mptcp_bpf_rr.skel.h"
 #include "mptcp_bpf_red.skel.h"
 #include "mptcp_bpf_burst.skel.h"
+#include "mptcp_bpf_stale.skel.h"
 
 #define NS_TEST "mptcp_ns"
 #define ADDR_1	"10.0.1.1"
@@ -1381,6 +1382,18 @@ static void test_burst(void)
 	mptcp_bpf_burst__destroy(skel);
 }
 
+static void test_stale(void)
+{
+	struct mptcp_bpf_stale *skel;
+
+	skel = mptcp_bpf_stale__open_and_load();
+	if (!ASSERT_OK_PTR(skel, "open_and_load: stale"))
+		return;
+
+	test_bpf_sched(skel->obj, "stale", WITH_DATA, WITHOUT_DATA);
+	mptcp_bpf_stale__destroy(skel);
+}
+
 void test_mptcp(void)
 {
 	if (test__start_subtest("base"))
@@ -1411,4 +1424,6 @@ void test_mptcp(void)
 		test_red();
 	if (test__start_subtest("burst"))
 		test_burst();
+	if (test__start_subtest("stale"))
+		test_stale();
 }
