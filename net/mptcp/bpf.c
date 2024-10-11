37204d18e43e80483f3e87ddb9f13803a80d7b11
Geliang Tang <tanggeliang@kylinos.cn>
2024-10-11 16:09:42 +0800

    Revert "mptcp: add sched_data helpers"
    Drop this patch.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>

--- net/mptcp/bpf.c
+++ net/mptcp/bpf.c
@@ -338,14 +338,6 @@ __bpf_kfunc static bool bpf_ipv6_addr_v4mapped(const struct mptcp_addr_info *a)
 	return false;
 }
 
-__bpf_kfunc struct mptcp_subflow_context *
-bpf_mptcp_subflow_ctx_by_pos(const struct mptcp_sched_data *data, unsigned int pos)
-{
-	if (pos >= MPTCP_SUBFLOWS_MAX)
-		return NULL;
-	return data->contexts[pos];
-}
-
 __bpf_kfunc static bool bpf_mptcp_subflow_queues_empty(struct sock *sk)
 {
 	return tcp_rtx_queue_empty(sk);
