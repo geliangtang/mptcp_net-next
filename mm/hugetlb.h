70b8d04807e926cae0aa0535666236026a338481
Geliang Tang <tanggeliang@kylinos.cn>
2025-07-28 18:14:18 +0800

    mm: hugetlb_interface: add demote attribute
    This patch adds demote attribute.
    
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>

--- mm/hugetlb.h
+++ mm/hugetlb.h
@@ -12,6 +12,7 @@
 
 int hugetlb_set_max_huge_pages(struct hstate *h, unsigned long count, int nid,
 			       nodemask_t *nodes_allowed);
+int hugetlb_demote_pool_page(struct hstate *h, nodemask_t *nodes_allowed);
 
 #endif /* _LINUX_HUGETLB_INTERNAL_H */
 
--- mm/hugetlb_interface.c
+++ mm/hugetlb_interface.c
@@ -263,8 +263,68 @@ static ssize_t demote_size_store(struct kobject *kobj,
 }
 HUGETLB_ATTR_RW(demote_size);
 
+static ssize_t demote_store(struct kobject *kobj,
+	       struct kobj_attribute *attr, const char *buf, size_t len)
+{
+	unsigned long nr_demote;
+	unsigned long nr_available;
+	nodemask_t nodes_allowed, *n_mask;
+	struct hstate *h;
+	int err;
+	int nid;
+
+	err = kstrtoul(buf, 10, &nr_demote);
+	if (err)
+		return err;
+	h = kobj_to_hstate(kobj, &nid);
+
+	if (nid != NUMA_NO_NODE) {
+		init_nodemask_of_node(&nodes_allowed, nid);
+		n_mask = &nodes_allowed;
+	} else {
+		n_mask = &node_states[N_MEMORY];
+	}
+
+	/* Synchronize with other sysfs operations modifying huge pages */
+	mutex_lock(&h->resize_lock);
+	spin_lock_irq(&hugetlb_lock);
+
+	while (nr_demote) {
+		long rc;
+
+		/*
+		 * Check for available pages to demote each time thorough the
+		 * loop as demote_pool_huge_page will drop hugetlb_lock.
+		 */
+		if (nid != NUMA_NO_NODE)
+			nr_available = h->free_huge_pages_node[nid];
+		else
+			nr_available = h->free_huge_pages;
+		nr_available -= h->resv_huge_pages;
+		if (!nr_available)
+			break;
+
+		rc = demote_pool_huge_page(h, n_mask, nr_demote);
+		if (rc < 0) {
+			err = rc;
+			break;
+		}
+
+		nr_demote -= rc;
+	}
+
+	spin_unlock_irq(&hugetlb_lock);
+	mutex_unlock(&h->resize_lock);
+
+	if (err)
+		return err;
+	return len;
+}
+HSTATE_ATTR_WO(demote);
+
 static struct attribute *hugetlb_demote_attrs[] = {
 	&demote_size_attr.attr,
+	&demote_attr.attr,
 	NULL,
 };
 
