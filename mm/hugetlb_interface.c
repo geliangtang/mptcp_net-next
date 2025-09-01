82917fa076f4489817235e0f8be8d1ada82a122a
Geliang Tang <geliang@kernel.org>
2025-09-01 10:49:08 +0800

    mm: hugetlb_interface: add nr_overcommit_hugepages sysctl
    This patch adds the nr_overcommit_hugepages sysctl entry, allowing users
    to set the overcommit limit for huge pages via sysctl. The entry is now
    part of hugetlb_interface.c.
    
    Co-developed-by: Muchun Song <songmuchun@bytedance.com>
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>
    Signed-off-by: Geliang Tang <geliang@kernel.org>

--- mm/hugetlb_interface.c
+++ mm/hugetlb_interface.c
@@ -729,6 +729,35 @@ static int hugetlb_mempolicy_sysctl_handler(const struct ctl_table *table, int w
 }
 #endif /* CONFIG_NUMA */
 
+static int hugetlb_overcommit_handler(const struct ctl_table *table, int write,
+		void *buffer, size_t *length, loff_t *ppos)
+{
+	struct hstate *h = &default_hstate;
+	unsigned long tmp;
+	int ret;
+
+	if (!hugepages_supported())
+		return -EOPNOTSUPP;
+
+	tmp = h->nr_overcommit_huge_pages;
+
+	if (write && hstate_is_gigantic(h))
+		return -EINVAL;
+
+	ret = proc_hugetlb_doulongvec_minmax(table, write, buffer, length, ppos,
+					     &tmp);
+	if (ret)
+		goto out;
+
+	if (write) {
+		spin_lock_irq(&hugetlb_lock);
+		h->nr_overcommit_huge_pages = tmp;
+		spin_unlock_irq(&hugetlb_lock);
+	}
+out:
+	return ret;
+}
+
 static const struct ctl_table hugetlb_table[] = {
 	{
 		.procname	= "nr_hugepages",
@@ -752,6 +781,13 @@ static const struct ctl_table hugetlb_table[] = {
 		.maxlen		= sizeof(gid_t),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
+	},
+	{
+		.procname	= "nr_overcommit_hugepages",
+		.data		= NULL,
+		.maxlen		= sizeof(unsigned long),
+		.mode		= 0644,
+		.proc_handler	= hugetlb_overcommit_handler,
 	},
 };
 
