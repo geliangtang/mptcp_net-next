06eb2a8a5a5ff2f20bccb82e57db8b1babde5a97
Geliang Tang <tanggeliang@kylinos.cn>
2025-09-12 14:34:47 +0800

    selftests: mptcp: sockopt: move read EOF to the end
    Moves EOF read handling to the end of process_one_client to ensure proper
    synchronization after processing all data transfers, preparing for TCP_INQ
    support in subsequent patches.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>

--- tools/testing/selftests/net/mptcp/mptcp_sockopt.c
+++ tools/testing/selftests/net/mptcp/mptcp_sockopt.c
@@ -660,15 +660,10 @@ static void process_one_client(int fd, int pipefd)
 	if (ret2 < 0)
 		die_perror("write");
 
-	/* wait for hangup */
-	ret3 = read(fd, buf, 1);
-	if (ret3 != 0)
-		xerror("expected EOF, got %lu", ret3);
-
 	do_getsockopts(&s, fd, ret, ret2);
-	if (s.mptcpi_rcv_delta != (uint64_t)ret + 1)
+	if (s.mptcpi_rcv_delta != (uint64_t)ret)
 		xerror("mptcpi_rcv_delta %" PRIu64 ", expect %" PRIu64 ", diff %" PRId64,
-		       s.mptcpi_rcv_delta, ret + 1, s.mptcpi_rcv_delta - (ret + 1));
+		       s.mptcpi_rcv_delta, ret, s.mptcpi_rcv_delta - ret);
 
 	/* be nice when running on top of older kernel */
 	if (s.pkt_stats_avail) {
@@ -688,6 +683,11 @@ static void process_one_client(int fd, int pipefd)
 			       s.last_sample.mptcpi_bytes_acked, ret,
 			       s.last_sample.mptcpi_bytes_acked - ret);
 	}
+
+	/* wait for hangup */
+	ret3 = read(fd, buf, 1);
+	if (ret3 != 0)
+		xerror("expected EOF, got %lu", ret3);
 
 	close(fd);
 }
