688de4f98d310ca56fcd08161fddcedd40c71444
Geliang Tang <tanggeliang@kylinos.cn>
2025-11-20 14:03:43 +0800

    nvme-tcp: implement host mptcp proto
    An MPTCP-specific version of struct nvme_tcp_proto is implemented,
    and it is assigned to ctrl->proto when the transport string is "mptcp".
    
    The socket option setting logic is similar to the target side, except that
    mptcp_sock_set_syncnt is newly defined for the host side.
    
    It sets the value on the first subflow socket of an MPTCP connection.
    The value is then synchronized to other newly created subflows in
    sync_socket_options().
    
    A separate nvme_mptcp_ctrl_ops structure with .name = "mptcp" is defined
    and used for MPTCP controllers.
    
    Cc: Hannes Reinecke <hare@suse.de>
    Co-developed-by: zhenwei pi <zhenwei.pi@linux.dev>
    Signed-off-by: zhenwei pi <zhenwei.pi@linux.dev>
    Co-developed-by: Hui Zhu <zhuhui@kylinos.cn>
    Signed-off-by: Hui Zhu <zhuhui@kylinos.cn>
    Co-developed-by: Gang Yan <yangang@kylinos.cn>
    Signed-off-by: Gang Yan <yangang@kylinos.cn>
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>

--- include/net/mptcp.h
+++ include/net/mptcp.h
@@ -249,6 +249,8 @@ void mptcp_sock_set_priority(struct sock *sk, u32 priority);
 void mptcp_sock_no_linger(struct sock *sk);
 
 void mptcp_sock_set_tos(struct sock *sk, int val);
+
+int mptcp_sock_set_syncnt(struct sock *sk, int val);
 #else
 
 static inline void mptcp_init(void)
@@ -345,6 +347,11 @@ static inline void mptcp_sock_set_priority(struct sock *sk, u32 priority) { }
 static inline void mptcp_sock_no_linger(struct sock *sk) { }
 
 static inline void mptcp_sock_set_tos(struct sock *sk, int val) { }
+
+static inline int mptcp_sock_set_syncnt(struct sock *sk, int val)
+{
+	return 0;
+}
 #endif /* CONFIG_MPTCP */
 
 #if IS_ENABLED(CONFIG_MPTCP_IPV6)
