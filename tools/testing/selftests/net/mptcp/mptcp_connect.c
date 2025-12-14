4b77b564f2d87cf62275dc804c5b3a61000b4a03
Geliang Tang <tanggeliang@kylinos.cn>
2025-12-14 09:23:35 +0800

    Revert "fallback debug"
    This reverts commit 382e2f3874371811cc564b2478e0fbe3c8ca91a5.

--- tools/testing/selftests/net/mptcp/mptcp_connect.c
+++ tools/testing/selftests/net/mptcp/mptcp_connect.c
@@ -269,7 +269,6 @@ static void set_mptfo(int fd, int pf)
 		perror("TCP_FASTOPEN");
 }
 
-#if 0
 static int fallback(int fd)
 {
 	int is_mptcp = 0;
@@ -281,7 +280,6 @@ static int fallback(int fd)
 
 	return !is_mptcp;
 }
-#endif
 
 static int do_setsockopt_tls(int fd)
 {
@@ -300,8 +298,8 @@ static int do_setsockopt_tls(int fd)
 	int so_buf = 6553500;
 	int err;
 
-	//if (fallback(fd))
-	//	return 0;
+	if (fallback(fd))
+		return 0;
 
 	if (cfg_disconnect || cfg_sockopt_types.mptfo)
 		return 0;
