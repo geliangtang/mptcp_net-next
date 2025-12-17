91161ea732474750f15a93b7cc073478da27ff3f
Geliang Tang <tanggeliang@kylinos.cn>
2025-12-17 14:05:42 +0800

    fix
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>

--- net/tls/tls_strp.c
+++ net/tls/tls_strp.c
@@ -601,7 +601,7 @@ void tls_strp_msg_done(struct tls_strparser *strp)
 {
 	WARN_ON(!strp->stm.full_len);
 
-	//pr_info("%s\n", __func__);
+	pr_info("%s strp->sk->sk_protocol=%d\n", __func__, strp->sk->sk_protocol);
 	if (likely(!strp->copy_mode))
 		sk_is_msk(strp->sk) ?
 		mptcp_read_done(strp->sk, strp->stm.full_len) :
