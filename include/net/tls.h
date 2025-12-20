83fbcbc209ee4fb458f810db4048c77c645a1828
Geliang Tang <tanggeliang@kylinos.cn>
2025-12-20 22:29:38 +0800

    tls: introduce tls protocol ops structure
    To extend MPTCP support based on TCP TLS, a tls_prot_ops structure has
    been introduced for TLS, encapsulating TCP-specific helpers within this
    structure.
    
    Add registering, validating and finding functions for this structure to
    add, validate and find a tls_prot_ops on the global list tls_prot_ops_list.
    
    Register TCP-specific structure tls_tcp_ops in tls_register().
    
    Co-developed-by: Gang Yan <yangang@kylinos.cn>
    Signed-off-by: Gang Yan <yangang@kylinos.cn>
    Co-developed-by: Zqiang <qiang.zhang@linux.dev>
    Signed-off-by: Zqiang <qiang.zhang@linux.dev>
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>

--- include/net/tls.h
+++ include/net/tls.h
@@ -224,6 +224,12 @@ struct tls_prot_info {
 	u16 tail_size;
 };
 
+struct tls_prot_ops {
+	struct sk_buff *(*recv_skb)(struct sock *sk, u32 *off);
+	void (*read_done)(struct sock *sk, size_t len);
+	bool (*epollin_ready)(const struct sock *sk, int targe);
+};
+
 struct tls_context {
 	/* read-only cache line */
 	struct tls_prot_info prot_info;
