33c867e730fb537d63b48d6c197858e428ec5961
Geliang Tang <tanggeliang@kylinos.cn>
2025-12-20 22:29:38 +0800

    tls: introduce tls protocol ops structure
    To extend MPTCP support based on TCP TLS, a tls_prot_ops structure has
    been introduced for TLS, encapsulating TCP-specific helpers within this
    structure.
    
    Add registering, validating and finding functions for this structure to
    add, validate and find a tls_prot_ops on the global list tls_prot_ops_list.
    
    Register TCP-specific structure tls_tcp_ops in tls_register().
    
    Co-developed-by: Gang Yan <yangang@kylinos.cn>
    Signed-off-by: Gang Yan <yangang@kylinos.cn>
    Co-developed-by: Zqiang <qiang.zhang@linux.dev>
    Signed-off-by: Zqiang <qiang.zhang@linux.dev>
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>

--- net/tls/tls_main.c
+++ net/tls/tls_main.c
@@ -133,6 +133,7 @@ static const struct proto *saved_mptcpv4_prot;
 static DEFINE_MUTEX(mptcpv4_prot_mutex);
 static struct proto tls_prots[TLS_NUM_PROTS][TLS_NUM_PROTO][TLS_NUM_CONFIG][TLS_NUM_CONFIG];
 static struct proto_ops tls_proto_ops[TLS_NUM_PROTS][TLS_NUM_PROTO][TLS_NUM_CONFIG][TLS_NUM_CONFIG];
+static struct tls_prot_ops tls_prot_ops[TLS_NUM_PROTO];
 static void build_protos(struct proto prot[TLS_NUM_CONFIG][TLS_NUM_CONFIG],
 			 const struct proto *base);
 
@@ -973,6 +974,20 @@ static void build_proto_ops(struct proto_ops ops[TLS_NUM_CONFIG][TLS_NUM_CONFIG]
 #endif
 }
 
+static struct sk_buff *tls_tcp_recv_skb(struct sock *sk, u32 *off)
+{
+	return tcp_recv_skb(sk, tcp_sk(sk)->copied_seq, off);
+}
+
+static void build_tls_proto_ops(int proto)
+{
+	if (proto == TLSTCP) {
+		tls_prot_ops[proto].recv_skb		= tls_tcp_recv_skb;
+		tls_prot_ops[proto].read_done		= tcp_read_done;
+		tls_prot_ops[proto].epollin_ready	= tcp_epollin_ready;
+	}
+}
+
 static void tls_build_proto(struct sock *sk)
 {
 	int proto = sk->sk_protocol == IPPROTO_MPTCP ? TLSMPTCP : TLSTCP;
@@ -987,6 +1002,7 @@ static void tls_build_proto(struct sock *sk)
 			build_protos(tls_prots[TLSV6][TLSTCP], prot);
 			build_proto_ops(tls_proto_ops[TLSV6][TLSTCP],
 					sk->sk_socket->ops);
+			build_tls_proto_ops(TLSTCP);
 			smp_store_release(&saved_tcpv6_prot, prot);
 		}
 		mutex_unlock(&tcpv6_prot_mutex);
@@ -999,6 +1015,7 @@ static void tls_build_proto(struct sock *sk)
 			build_protos(tls_prots[TLSV4][TLSTCP], prot);
 			build_proto_ops(tls_proto_ops[TLSV4][TLSTCP],
 					sk->sk_socket->ops);
+			build_tls_proto_ops(TLSTCP);
 			smp_store_release(&saved_tcpv4_prot, prot);
 		}
 		mutex_unlock(&tcpv4_prot_mutex);
