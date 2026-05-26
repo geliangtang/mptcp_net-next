3157e622b472bca20208631500cb3b7adb65bfbf
Geliang Tang <tanggeliang@kylinos.cn>
2026-05-26 10:02:24 +0800

    nvme-fabrics: add network namespace tracking to ctrl options
    Add net field to struct nvmf_ctrl_options to store the network namespace
    of the controller during option parsing. Use the saved namespace for
    all network-dependent operations:
    
    - sock_create_kern(): create socket in the correct namespace
    - inet_pton_with_scope(): parse addresses with proper scope resolution
    - __dev_get_by_name(): validate host interface in the correct namespace
    
    This fixes reconnection failures in container environments where the
    reconnection workqueue runs in init_net instead of the original
    namespace.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>

--- drivers/nvme/host/tcp.c
+++ drivers/nvme/host/tcp.c
@@ -1809,7 +1809,7 @@ static int nvme_tcp_alloc_queue(struct nvme_ctrl *nctrl, int qid,
 		queue->cmnd_capsule_len = sizeof(struct nvme_command) +
 						NVME_TCP_ADMIN_CCSZ;
 
-	ret = sock_create_kern(current->nsproxy->net_ns,
+	ret = sock_create_kern(ctrl->ctrl.opts->net,
 			ctrl->addr.ss_family, SOCK_STREAM,
 			ctrl->proto->protocol, &queue->sock);
 	if (ret) {
@@ -2985,7 +2985,7 @@ static struct nvme_tcp_ctrl *nvme_tcp_alloc_ctrl(struct device *dev,
 		opts->mask |= NVMF_OPT_TRSVCID;
 	}
 
-	ret = inet_pton_with_scope(&init_net, AF_UNSPEC,
+	ret = inet_pton_with_scope(opts->net, AF_UNSPEC,
 			opts->traddr, opts->trsvcid, &ctrl->addr);
 	if (ret) {
 		pr_err("malformed address passed: %s:%s\n",
@@ -2994,7 +2994,7 @@ static struct nvme_tcp_ctrl *nvme_tcp_alloc_ctrl(struct device *dev,
 	}
 
 	if (opts->mask & NVMF_OPT_HOST_TRADDR) {
-		ret = inet_pton_with_scope(&init_net, AF_UNSPEC,
+		ret = inet_pton_with_scope(opts->net, AF_UNSPEC,
 			opts->host_traddr, NULL, &ctrl->src_addr);
 		if (ret) {
 			pr_err("malformed src address passed: %s\n",
@@ -3004,7 +3004,7 @@ static struct nvme_tcp_ctrl *nvme_tcp_alloc_ctrl(struct device *dev,
 	}
 
 	if (opts->mask & NVMF_OPT_HOST_IFACE) {
-		if (!__dev_get_by_name(&init_net, opts->host_iface)) {
+		if (!__dev_get_by_name(opts->net, opts->host_iface)) {
 			pr_err("invalid interface passed: %s\n",
 			       opts->host_iface);
 			ret = -ENODEV;
