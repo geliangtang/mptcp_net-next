b6a3e0fe469f0cf0f34bd2a94e405c67a9bec18d
Geliang Tang <tanggeliang@kylinos.cn>
2026-07-20 17:32:06 +0800

    run multi_chunk_sendfile only
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>

--- tools/testing/selftests/net/mptcp/mptcp_tls.sh
+++ tools/testing/selftests/net/mptcp/mptcp_tls.sh
@@ -40,6 +40,7 @@ trap cleanup EXIT
 
 init
 
+#ip netns exec "$ns1" ./tls -t multi_chunk_sendfile &
 ip netns exec "$ns1" ./tls -v 12_aes_gcm_mptcp \
 			   -v 13_aes_gcm_mptcp \
 			   -v 12_chacha_mptcp \
