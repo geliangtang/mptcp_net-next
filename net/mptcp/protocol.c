541b524b96fe65cc701338643191134acb46fa94
Geliang Tang <tanggeliang@kylinos.cn>
2026-08-26 10:25:33 +0800

    mptcp: export and generalize tcp_bpf_ioctl
    Export tcp_bpf_ioctl() and generalize it to use protocol-specific
    operations (sk->sk_prot->ioctl and sk->sk_socket->ops->peek_len) instead
    of hard-coded tcp_ioctl() and tcp_inq(). Wire it into the MPTCP BPF
    protocol structures so that SIOCINQ returns correct results accounting
    for data in the psock ingress queue.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -4248,7 +4248,7 @@ static int mptcp_ioctl_outq(const struct mptcp_sock *msk, u64 v)
 	return (int)delta;
 }
 
-static int mptcp_ioctl(struct sock *sk, int cmd, int *karg)
+int mptcp_ioctl(struct sock *sk, int cmd, int *karg)
 {
 	struct mptcp_sock *msk = mptcp_sk(sk);
 	bool slow;
--- net/mptcp/protocol.h
+++ net/mptcp/protocol.h
@@ -1412,6 +1412,7 @@ static inline void mptcp_join_cookie_init(void) {}
 extern struct proto mptcp_prot;
 int mptcp_recvmsg(struct sock *sk, struct msghdr *msg, size_t len, int flags);
 int mptcp_sendmsg(struct sock *sk, struct msghdr *msg, size_t len);
+int mptcp_ioctl(struct sock *sk, int cmd, int *karg);
 
 #ifdef CONFIG_BPF_SYSCALL
 int mptcp_bpf_update_proto(struct sock *sk, struct sk_psock *psock,
