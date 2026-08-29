152547fc914902df4833b7730f99c67f9d6c018d
Geliang Tang <tanggeliang@kylinos.cn>
2026-08-29 20:29:27 +0800

    mptcp_pm_subflow_track_stale
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>

--- net/mptcp/pm.c
+++ net/mptcp/pm.c
@@ -1103,6 +1103,21 @@ mptcp_pm_subflow_chk_stale(const struct mptcp_sock *msk, struct sock *ssk)
 	}
 }
 
+void mptcp_pm_subflow_track_stale(const struct mptcp_sock *msk,
+				  struct sock *ssk)
+{
+	struct mptcp_subflow_context *subflow = mptcp_subflow_ctx(ssk);
+	u32 rcv_tstamp = READ_ONCE(tcp_sk(ssk)->rcv_tstamp);
+
+	if (subflow->stale_rcv_tstamp == rcv_tstamp &&
+	    subflow->stale_count < U8_MAX) {
+		subflow->stale_count++;
+		mptcp_pm_subflow_chk_stale(msk, ssk);
+	} else {
+		subflow->stale_rcv_tstamp = rcv_tstamp;
+	}
+}
+
 void mptcp_pm_chk_stale(const struct mptcp_sock *msk)
 {
 	struct mptcp_subflow_context *subflow;
