41b3ae9d5c9fbc4b76b53ff98e44cb7293813f8f
Geliang Tang <tanggeliang@kylinos.cn>
2026-09-12 18:46:15 +0800

    fastopen fix
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>

--- tools/testing/selftests/net/mptcp/mptcp_connect.sh
+++ tools/testing/selftests/net/mptcp/mptcp_connect.sh
@@ -969,6 +969,9 @@ TEST_GROUP="loopback v4"
 run_tests_lo "$ns1" "$ns1" 10.0.1.1 1
 stop_if_error "Could not even run loopback test"
 
+run_tests_mptfo
+log_if_error "Tests with MPTFO have failed"
+
 TEST_GROUP="loopback v6"
 run_tests_lo "$ns1" "$ns1" dead:beef:1::1 1
 stop_if_error "Could not even run loopback v6 test"
@@ -1007,8 +1010,8 @@ run_tests_peekmode "saveAfterPeek"
 log_if_error "Tests with peek mode have failed"
 
 # MPTFO (MultiPath TCP Fatopen tests)
-run_tests_mptfo
-log_if_error "Tests with MPTFO have failed"
+#run_tests_mptfo
+#log_if_error "Tests with MPTFO have failed"
 
 # connect to ns4 ip address, ns2 should intercept/proxy
 run_test_transparent 10.0.3.1 "tproxy ipv4"
